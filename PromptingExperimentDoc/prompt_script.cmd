6ef2cf25e8c5751e09de04dd6b472f2b7d0ffe47
Car0linaaS <112877280+Car0linaaS@users.noreply.github.com>
2025-04-17 17:43:25 +0200

    Remove quotes for paths

--- PromptingExperimentDoc/prompt_script.cmd
+++ PromptingExperimentDoc/prompt_script.cmd
@@ -2,24 +2,27 @@
 @REM Set up a local environment for variables etc.
 setlocal
 
-@REM %%F will give us promptX, e.g. prompt1, 
-for %%F in ("LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\*.property") do (
+@REM %%F will give us prompot ID number, e.g. prompt1
+for %%F in (LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\*.property) do (
     echo Running RMC with %%F
+    echo.     
 
-    @REM Create output folder
-    if not exist "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF" mkdir "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF"
-    if not exist "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output" mkdir "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output"
+    @REM Create output folders
+    if not exist LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF mkdir LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF
+    if not exist LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output mkdir LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output
     
-    for %%G in ("RebecaCodeScenarios\Rule19RebecaCodeScenarios\*.rebeca") do (
+    @REM %%G will give us the "long" scenario name, e.g. S1VEL11LEN160DRA20HAZnPDaEOa_S2VEL11LEN160DRA20HAZnPDaEOa_theMapUCaSHCLaSAFS3_FOGa
+    for %%G in (RebecaCodeScenarios\Rule19RebecaCodeScenarios\*.rebeca) do (
         echo Running RMC with %%G
+        echo.       
 
-        if not exist "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG" mkdir "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG"
-        java -jar "rmc-2.13.jar" -s "%%G" -p "%%F" -o "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG" -e TIMED_REBECA -x
-        g++ "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG\*.cpp" -w -o "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG\%%~nF_%%~nG"
+        if not exist LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG mkdir LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG
+        java -jar rmc-2.13.jar -s %%G -p %%F -o LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG -e TIMED_REBECA -x
+        g++ LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG\*.cpp -w -o LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG\%%~nF_%%~nG.exe
         
         
-        "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG\%%~nF_%%~nG.exe" > "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output\%%~nF_%%~nG.txt"
-        move statespace.xml "LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output\%%~nF_%%~nG.xml"
+        LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG\%%~nF_%%~nG.exe > LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output\%%~nF_%%~nG.txt
+        move statespace.xml LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output\%%~nF_%%~nG.xml
     )
 
 )
