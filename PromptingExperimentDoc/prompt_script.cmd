98481e2f6e2227c96a468339fb3e69980a592907
SkyQueenSE <Jackiie.samuelsson@gmail.com>
2025-04-17 18:08:07 +0200

    Update script

--- PromptingExperimentDoc/prompt_script.cmd
+++ PromptingExperimentDoc/prompt_script.cmd
@@ -1,28 +1,64 @@
 @echo off
 @REM Set up a local environment for variables etc.
-setlocal
+setlocal enabledelayedexpansion
+
+set "BASE=LLMLeg2Reb\V1\Rule19"
+set "SCEN_DIR=RebecaCodeScenarios\Rule19RebecaCodeScenarios"
 
 @REM %%F will give us prompot ID number, e.g. prompt1
-for %%F in (LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\*.property) do (
-    echo Running RMC with %%F
-    echo.     
+for %%F in ("%BASE%\*.property") do (
+    set "PROMPT=%%~nF"
+    echo.
+    echo === Processing prompt: !PROMPT! ===
 
     @REM Create output folders
-    if not exist LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF mkdir LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF
-    if not exist LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output mkdir LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output
+    if not exist "%BASE%\RMCout_!PROMPT!"  mkdir "%BASE%\RMCout_!PROMPT!"
+    if not exist "%BASE%\statespace_output" mkdir "%BASE%\statespace_output"
     
     @REM %%G will give us the "long" scenario name, e.g. S1VEL11LEN160DRA20HAZnPDaEOa_S2VEL11LEN160DRA20HAZnPDaEOa_theMapUCaSHCLaSAFS3_FOGa
-    for %%G in (RebecaCodeScenarios\Rule19RebecaCodeScenarios\*.rebeca) do (
-        echo Running RMC with %%G
+    for %%G in ("%SCEN_DIR%\*.rebeca") do (
+        set "SCEN=%%~nG"
+        set "OUTDIR=%BASE%\RMCout_!PROMPT!\!SCEN!"
         echo.       
 
-        if not exist LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG mkdir LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG
-        java -jar rmc-2.13.jar -s %%G -p %%F -o LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG -e TIMED_REBECA -x
-        g++ LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG\*.cpp -w -o LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG\%%~nF_%%~nG.exe
-        
-        
-        LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\RMCoutput_%%~nF\%%~nG\%%~nF_%%~nG.exe > LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output\%%~nF_%%~nG.txt
-        move statespace.xml LLMTransformation_Legata2Rebeca_properties\Version1_Zero_shot\Rule19\statespace_output\%%~nF_%%~nG.xml
+        if not exist "!OUTDIR!" mkdir "!OUTDIR!"
+        java -jar rmc-2.13.jar -s "%%G" -p "%%F" -o "!OUTDIR!" -e TIMED_REBECA -x
+        if errorlevel 1 (
+            echo ERROR: RMC failed on %%G with code %ERRORLEVEL%
+            exit /b 1
+        )
+
+            @rem ── Compile (fixed wildcard expansion)
+            pushd "!OUTDIR!" || exit /b 1
+            rem Make sure there are .cpp files to compile
+            dir /b *.cpp >nul 2>&1
+            if errorlevel 1 (
+                echo ERROR: No .cpp files in "!OUTDIR!"
+                popd
+                exit /b 1
+            )
+            @rem Now compile: use unquoted wildcard so cmd expands it
+            g++ *.cpp -w -o "..\!PROMPT!_!SCEN!.exe"
+            if errorlevel 1 (
+                echo ERROR: g++ failed to compile in "!OUTDIR!"
+                popd
+                exit /b 1
+            )
+            popd
+
+
+
+        @rem ── Execute & capture
+        !OUTDIR!\!PROMPT!_!SCEN!.exe > "%BASE%\statespace_output\!PROMPT!_!SCEN!.txt"
+        if errorlevel 1 (
+            echo WARNING: execution of !PROMPT!_!SCEN! returned %ERRORLEVEL%
+        )
+
+        @rem ── Move the XML, if it was generated
+        if exist statespace.xml (  
+            move statespace.xml "%BASE%\statespace_output\!PROMPT!_!SCEN!.xml"
+        )
+
     )
 
 )
